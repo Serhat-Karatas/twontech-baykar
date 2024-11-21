4af85d14600097ab0886bc64b021c8af1c1e601e
Serhat Karataş <2serhatkaratas2@gmail.com>
2024-11-21 18:18:48 +0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Kişisel Bütçe ve Harcama Takip Uygulaması ",
+  description:
+    "Kişisel bütçe ve harcama takip uygulaması ile harcamalarınızı kontrol altına alın.",
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="tr">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
