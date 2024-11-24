31916d8e23afa75bb35ff0a1649590236f233611
Serhat Karataş <2serhatkaratas2@gmail.com>
2024-11-24 19:30:17 +0300

    fix: dark mod bug fixed

--- container/Home.js
+++ container/Home.js
@@ -1,21 +1,31 @@
 "use client";
-import React from "react";
-import { useBudget } from "@/app/context/Context";
+import React, { useEffect } from "react";
 import Banner from "@/components/banner/Banner";
 import Income from "@/components/income/Income";
 import Category from "@/components/category/Category";
 import Expense from "@/components/expense/Expense";
 import Analysis from "@/components/analysis/Analysis";
 import Bonus from "@/components/bonus/Bonus";
+import { useBudget } from "@/app/context/Context";
 
 const Home = () => {
   const { darkMode } = useBudget();
 
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [darkMode]);
+
   return (
     <main
-      className={`flex flex-col items-center gap-28 ${
-        darkMode ? "dark bg-gray-900 text-white" : "bg-white text-black"
-      }`}
+      className="flex flex-col items-center gap-28"
+      style={{
+        backgroundColor: darkMode ? "rgb(17, 24, 39)" : "rgb(255, 255, 255)",
+        color: darkMode ? "rgb(255, 255, 255)" : "rgb(0, 0, 0)",
+      }}
     >
       <Banner />
       <Income />
